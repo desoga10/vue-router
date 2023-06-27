b70d7a493765e7099972232f268d6706f20bfb69
desoga10 <done2stay@gmail.com>
2023-06-27 01:40:48 +0100

    Initial Commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima mollitia aliquid corporis dolores, rem odit similique dicta quia inventore rerum illum iure doloribus et doloremque est sit, at incidunt ullam, suscipit eum totam! Earum vero repellat magni possimus maxime temporibus, fuga explicabo accusamus id eaque nihil magnam illo eius quod!</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam animi molestias officiis, consectetur neque blanditiis nemo officia exercitationem! Blanditiis atque tenetur, odio reiciendis doloribus dicta recusandae, possimus iusto, nemo quas minima porro. Tempora at eveniet, ex ad, porro, voluptate perferendis nemo id ducimus reiciendis sit. Est minus tempore incidunt porro?</p>
+  </main>
+</template>
