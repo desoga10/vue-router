22fe59d42c90147e5d8e106a7dc612a00b5c922e
desoga10 <done2stay@gmail.com>
2023-06-27 02:28:30 +0100

    Looked into Route Parameters

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,18 @@
+<template >
+  <div>
+<h1>Job Details Page</h1>
+<p>The Job ID is {{ id }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+    id: this.$route.params.id
+  }
+}
+}
+</script>
+<style >
+
+</style>
