9151d82f88ae1d2445d9412d24fb282808e3033c
desoga10 <done2stay@gmail.com>
2023-07-01 18:23:50 +0100

    Integrated Data from JSON-SERVER

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,19 +1,30 @@
 <template >
-  <div>
-<h1>Job Details Page</h1>
-<p>The Job ID is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>The Job ID is {{ id }}</p>
+    <p>{{ job.description }}</p>
+  </div>
+  <div v-else>
+    <p>Loading Job Description...</p>
   </div>
 </template>
 <script>
 export default {
   props: ['id'],
-//   data() {
-//     return {
-//     id: this.$route.params.id
-//   }
-// }
+  data() {
+    return {
+      job: null
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs/' + this.id)
+      .then(response => response.json())
+      .then(data => {
+        this.job = data
+      }).catch(error => {
+        console.log(error)
+      })
+  }
 }
 </script>
-<style >
-
-</style>
+<style ></style>
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,42 +1,32 @@
 <template lang="">
-  <div>
 <h1>jobs</h1>
-<div v-for="job in jobs" :key="job.id" class="job">
+<div v-if="jobs.length">
+  <div v-for="job in jobs" :key="job.id" class="job">
 <router-link :to="{name: 'JobDetails', params:{id: job.id}}">
   <h2>{{ job.title }}</h2>
 </router-link>
 </div>
-  </div>
+</div>
+<div v-else>
+ <p>Loading Jobs...</p>
+</div>
 </template>
 <script>
 export default {
   data() {
     return {
-      jobs: [
-        {
-          id: 1,
-          title: 'Web Developer',
-          company: 'Google',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-          created_at: '2021-09-01'
-        },
-        {
-          id: 2,
-          title: 'Web Designer',
-          company: 'Facebook',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-          created_at: '2021-09-01'
-        },
-        {
-          id: 3,
-          title: 'Frontend Developer',
-          company: 'Apple',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-          created_at: '2021-09-01'
-        }
-      ]
+      jobs: []
     }
   },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+      .then(response => response.json())
+      .then(data => {
+        this.jobs = data
+      }).catch(error => {
+        console.log(error)
+      })
+  }
 }
 </script>
 <style >
